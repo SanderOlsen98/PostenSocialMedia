d5e79e8717c1e80f7e5228bb9cc448f4f8e45c17
SanderOlsen98 <sanderlolsen@gmail.com>
2023-01-17 16:56:49 +0100

    new updates

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: "string",
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: "string",
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: "string",
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: "string",
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: "string",
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    oppupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
